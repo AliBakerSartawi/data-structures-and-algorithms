4490d1c7180fb55a43905c07671a9eec9c62b42f
AliBakerSartawi <alisartawi@live.com>
2021-05-21 06:07:15 +0300

    stack&queue done, readme remains

--- code-challenges/stacksAndQueues/stacks-and-queues.js
+++ code-challenges/stacksAndQueues/stacks-and-queues.js
@@ -1,5 +1,8 @@
 'use strict';
 
+
+// ------------------------STACK------------------------
+
 class Stack {
   constructor() {
     this.storage = {};
@@ -8,7 +11,7 @@ class Stack {
   }
 
   push(value) {
-    let newNode = new StackNode(value, this.top || null);
+    let newNode = new Node(value, this.top || null);
     this.size++;
     this.storage[this.size] = newNode;
     this.top = this.storage[this.size];
@@ -16,15 +19,17 @@ class Stack {
 
   pop() {
     if (this.isEmpty()) throw new Error('Cannot pop() from empty stack');
+
     let popped = this.top;
     delete this.storage[this.size]; // deleting the top from storage
     this.size--;
-    this.top = this.storage[this.size];
+    this.top = this.storage[this.size] || null;
     return popped;
   }
 
   peek() {
     if (this.isEmpty()) throw new Error('Cannot peek() empty stack');
+
     return this.top;
   }
 
@@ -33,21 +38,62 @@ class Stack {
   }
 }
 
-class StackNode {
+// ------------------------QUEUE------------------------
+
+class Queue {
+  constructor() {
+    this.storage = {};
+    this.front = null;
+    this.rear = null;
+    this.frontIndex = 0;
+    this.rearIndex = 0;
+    this.size = 0;
+  }
+
+  enqueue(value) {
+    let newNode = new Node(value, null); // next is always null for a new node
+    this.storage[this.rearIndex] = newNode;
+    this.front = this.storage[this.frontIndex];
+    this.rear = this.storage[this.rearIndex];
+    this.rearIndex++;
+    this.size = this.rearIndex - this.frontIndex;
+
+    // setting the next property for the previous node
+    if (this.size > 1) {
+      this.storage[this.rearIndex - 2].next = this.storage[this.rearIndex - 1];
+    }
+  }
+
+  dequeue() {
+    if (this.isEmpty()) throw new Error('Cannot dequeue() empty queue');
+
+    let dequeued = this.front;
+    delete this.storage[this.frontIndex];
+    this.frontIndex++;
+    this.front = this.storage[this.frontIndex] || null;
+    this.size = this.rearIndex - this.frontIndex;
+
+    return dequeued;
+  }
+
+  peek() {
+    if (this.isEmpty()) throw new Error('Cannot peek() empty queue');
+
+    return this.front;
+  }
+
+  isEmpty() {
+    return this.size === 0 ? true : false;
+  }
+}
+
+// ------------ONE NODE CLASS FOR STACK AND QUEUE------------
+
+class Node {
   constructor(value, next) {
     this.value = value;
     this.next = next;
   }
-}
-
-// -------------------------------
-
-class Queue {
-
-}
-
-class QueueNode {
-  
 }
 
 // -------------------------------
--- code-challenges/stacksAndQueues/stacks-and-queues.test.js
+++ code-challenges/stacksAndQueues/stacks-and-queues.test.js
@@ -84,4 +84,125 @@ describe(':::::Stack:::::', () => {
     stack.push(10);
     expect(stack.isEmpty()).toEqual(false);
   });
+
+  it('#push() and pop() until empty --> top will be null, not undefined --> can push again', () => {
+    let stack = new Stack();
+    stack.push(10);
+    stack.push(20);
+    stack.push(30);
+    stack.pop();
+    stack.pop();
+    stack.pop();
+    expect(stack.isEmpty()).toEqual(true);
+    expect(stack.size).toEqual(0);
+    expect(stack.top).toEqual(null);
+
+    stack.push(10);
+    expect(stack.size).toEqual(1);
+    expect(stack.storage[1].value).toEqual(10);
+    expect(stack.storage[1].next).toEqual(null);
+    expect(stack.top.value).toEqual(10);
+
+    stack.push(20);
+    expect(stack.size).toEqual(2);
+    expect(stack.storage[2].value).toEqual(20);
+    expect(stack.storage[2].next.value).toEqual(10);
+    expect(stack.top.value).toEqual(20);
+  });
+});
+
+describe(':::::Queue:::::', () => {
+  it('#constructor creates empty Queue', () => {
+    let queue = new Queue();
+    expect(queue instanceof Queue).toBeTruthy();
+    expect(queue.front).toBeNull();
+    expect(queue.rear).toBeNull();
+    expect(queue.size).toEqual(0);
+  });
+
+  it('#enqueue() adds to the queue --> next & front & rear are set properly', () => {
+    let q = new Queue();
+    q.enqueue(10);
+    q.enqueue(20);
+    q.enqueue(30);
+    expect(q.size).toEqual(3);
+    expect(q.front.value).toEqual(10);
+    expect(q.front.next.value).toEqual(20);
+    expect(q.front.next.next.value).toEqual(30);
+    expect(q.rear.value).toEqual(30);
+  });
+
+  it('#dequeue() throws error if queue is empty', () => {
+    let q = new Queue();
+    expect(() => q.dequeue()).toThrow('Cannot dequeue() empty queue');
+  });
+
+  it('#dequeue() removes from the front of the queue --> front is set properly', () => {
+    let q = new Queue();
+    q.enqueue(10);
+    q.enqueue(20);
+    q.enqueue(30);
+    expect(q.front.value).toEqual(10);
+
+    q.dequeue();
+    expect(q.front.value).toEqual(20);
+
+    q.dequeue();
+    expect(q.front.value).toEqual(30);
+    expect(q.rear.value).toEqual(30);
+  });
+
+  it('#peek() throws error if queue is empty', () => {
+    let q = new Queue();
+    expect(() => q.peek()).toThrow('Cannot peek() empty queue');
+  });
+
+  it('#peek() returns the node in the front', () => {
+    let q = new Queue();
+    q.enqueue(10);
+    q.enqueue(20);
+    q.enqueue(30);
+    expect(q.peek().value).toEqual(10);
+    q.dequeue();
+    expect(q.peek().value).toEqual(20);
+    q.dequeue();
+    expect(q.peek().value).toEqual(30);
+  });
+
+  it('#isEmpty() return true if queue is empty', () => {
+    let q = new Queue();
+    expect(q.isEmpty()).toEqual(true);
+  });
+
+  it('#isEmpty() return false if queue is not empty', () => {
+    let q = new Queue();
+    q.enqueue(10);
+    expect(q.isEmpty()).toEqual(false);
+  });
+
+  it('#enqueue() & dequeue() until queue is empty --> front will be null, not undefined --> can enqueue again', () => {
+    let q = new Queue();
+    q.enqueue(10);
+    q.enqueue(20);
+    q.enqueue(30);
+    q.dequeue();
+    q.dequeue();
+    q.dequeue();
+    expect(q.isEmpty()).toEqual(true);
+    expect(q.size).toEqual(0);
+    expect(q.front).toEqual(null);
+
+    let num = 0;
+    console.log(++num);
+    q.enqueue(10);
+    console.log(++num);
+    console.log(q);
+    q.enqueue(20);
+    q.enqueue(30);
+    expect(q.size).toEqual(3);
+    expect(q.front.value).toEqual(10);
+    expect(q.front.next.value).toEqual(20);
+    expect(q.front.next.next.value).toEqual(30);
+    expect(q.rear.value).toEqual(30);
+  });
 });
