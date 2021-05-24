f30d07e8e88ab546210a71e234cd17837d06f7fd
AliBakerSartawi <alisartawi@live.com>
2021-05-25 01:21:06 +0300

    README and testing done

---
+++ code-challenges/fifoAnimalShelter/README.md
@@ -0,0 +1,18 @@
+# FIFO Animal Shelter
+
+[**FIFO AnimalShelter**](./fifo-animal-shelter.js)
+
+**Challenge**:
+- Create a `FIFO` structure that has two types (cats and dogs)
+
+**Approach & Efficiency**:
+- `FIFO` means `queue`, so I used my previously created `Queue` class
+  - I created two instances of `Queue` inside the constructor of `AnimalShelter`, one for cats and one for dogs
+  - With two queues, the `dequeue` method will work properly on each type
+  - If we had one type, we would face a problem of having to traverse the queue to search for the desired type (the `pref` parameter), and this would oppose the `FIFO` structure as well
+
+**Solution**:
+
+*run: npm test fifo-animal-shelter*
+
+![AnimalShelter img](../../assets/fifo-animal-shelter.png)
--- code-challenges/fifoAnimalShelter/fifo-animal-shelter.js
+++ code-challenges/fifoAnimalShelter/fifo-animal-shelter.js
@@ -10,6 +10,9 @@ class AnimalShelter {
   }
 
   enqueue(animal) {
+
+    if (!animal.type || (animal.type !== 'dog' && animal.type !== 'cat')) throw new Error('Animal type was not specified!');
+
     if (animal.type === 'dog') {
       this.dogShelter.enqueue(animal);
     }
@@ -20,26 +23,27 @@ class AnimalShelter {
   }
 
   dequeue(pref) {
+
     if (pref === 'dog') {
-      return this.dogShelter.front; // or .peek()
+      return this.dogShelter.dequeue();
     }
 
     if (pref === 'cat') {
-      return this.catShelter.front; // or .peek()
+      return this.catShelter.dequeue();
     }
 
     //// stretch goal: if pref === undefined, return whichever
     //// animal has been waiting in the shelter the longest
     if (this.catShelter.size < this.dogShelter.size) {
-      return this.dogShelter.front;
+      return this.dogShelter.dequeue();
     }
 
     if (this.catShelter.size > this.dogShelter.size) {
-      return this.catShelter.front;
+      return this.catShelter.dequeue();
     }
 
     if (this.catShelter.size === this.dogShelter.size) {
-      return [this.dogShelter.front, this.catShelter.front];
+      return [this.dogShelter.dequeue(), this.catShelter.dequeue()];
     }
   }
 }
