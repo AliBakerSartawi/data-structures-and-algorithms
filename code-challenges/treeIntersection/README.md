e161b5b38166a344defc1609d396d5b5ec88262e
AliBakerSartawi <alisartawi@live.com>
2021-06-21 21:10:58 +0300

    function done

---
+++ code-challenges/treeIntersection/README.md
@@ -0,0 +1,15 @@
+# Repeated Word
+
+[**Repeated Word**](./repeated-word.js)
+
+**Challenge**:
+- Create a function that takes in a lengthy string, and returns the first word to be repeated
+
+**Approach & Efficiency**: 
+- regex and a for loop were used to find the result
+
+**Solution**:
+
+*run: npm test repeated-word*
+
+![repeatedWord img](../../assets/repeated-word.png)
---
+++ code-challenges/treeIntersection/tree-intersection.test.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const BST = require('../tree/tree').BinarySearchTree;
+const treeIntersection = require('./tree-intersection');
+
+describe('treeIntersection', () => {
+
+  let bst1, bst2, bst3;
+  beforeEach(() => {
+    bst1 = new BST(10);
+    bst1.add(3);
+    bst1.add(12);
+    bst1.add(2);
+    bst1.add(36);
+    bst1.add(28);
+    bst1.add(39);
+
+    bst2 = new BST(20);
+    bst2.add(4);
+    bst2.add(12);
+    bst2.add(5);
+    bst2.add(36);
+    bst2.add(46);
+    bst2.add(39);
+
+    bst3 = new BST(30);
+    bst3.add(41);
+    bst3.add(121);
+    bst3.add(51);
+    bst3.add(361);
+    bst3.add(461);
+    bst3.add(391);
+  });
+
+  it('returns array of shared values', () => {
+    expect(treeIntersection(bst1, bst2)).toEqual([12, 36, 39]);
+  });
+});
+
+
+
