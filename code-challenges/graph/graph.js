057f2a827669016fd9bcf746761c61653f413878
AliBakerSartawi <alisartawi@live.com>
2021-06-25 03:11:27 +0300

    tests half done

--- code-challenges/graph/graph.js
+++ code-challenges/graph/graph.js
@@ -18,10 +18,10 @@ class Graph {
     this.adjacencyList = new Map();
   }
 
-  createVertex(value) {
-    const vertex = new Vertex(value);
-    this.adjacencyList.set(vertex, []);
-  }
+  // createVertex(value) {
+  //   const vertex = new Vertex(value);
+  //   this.adjacencyList.set(vertex, []);
+  // }
 
   addVertex(vertex) {
     this.adjacencyList.set(vertex, []);
@@ -30,8 +30,7 @@ class Graph {
 
   addDirectedEdge(start, end, weight) {
     if (!this.adjacencyList.has(start) || !this.adjacencyList.has(end)) {
-      console.log('NON-EXISTENT VERTEX SITUATION 😱');
-      return;
+      throw new Error('NON-EXISTENT VERTEX SITUATION 😱');
     }
 
     const edge = new Edge(end, weight || null);
@@ -39,29 +38,29 @@ class Graph {
   }
 
   getVertices() {
-    if (!this.size()) return null;
+    if (!this.size()) throw new Error('GRAPH IS EMPTY 😱');
 
     const collection = [];
     for (let k of this.adjacencyList.keys()) {
-      console.log(k.value);
+      // console.log(k.value);
       collection.push(k.value);
     }
     return collection;
   }
 
   getUniqueVertices() {
-    if (!this.size()) return null;
+    if (!this.size()) throw new Error('GRAPH IS EMPTY 😱');
 
     const collection = new Set();
     for (let k of this.adjacencyList.keys()) {
-      console.log(k);
+      // console.log(k);
       collection.add(k.value);
     }
     return collection;
   }
 
   getNeighbors(vertex) {
-    console.log(vertex);
+    // console.log(vertex);
     return this.adjacencyList.get(vertex) || null;
   }
 
@@ -70,34 +69,40 @@ class Graph {
   }
 }
 
-const g = new Graph();
+// const g = new Graph();
 
-const zero = new Vertex(0);
-const one = new Vertex(1);
-const two = new Vertex(2);
-const twoToo = new Vertex(2);
-const three = new Vertex(3);
-const four = new Vertex(4);
-const fourToo = new Vertex(4);
-const five = new Vertex(5);
+// const zero = new Vertex(0);
+// const one = new Vertex(1);
+// const two = new Vertex(2);
+// const twoToo = new Vertex(2);
+// const three = new Vertex(3);
+// const four = new Vertex(4);
+// const fourToo = new Vertex(4);
+// const five = new Vertex(5);
 
-g.addVertex(zero);
-g.addVertex(one);
-g.addVertex(two);
-g.addVertex(twoToo);
-g.addVertex(three);
-g.addVertex(four);
-g.addVertex(fourToo);
-g.addVertex(five);
+// g.addVertex(zero);
+// g.addVertex(one);
+// g.addVertex(two);
+// g.addVertex(twoToo);
+// g.addVertex(three);
+// g.addVertex(four);
+// g.addVertex(fourToo);
+// g.addVertex(five);
 
-g.addDirectedEdge(zero, two);
-g.addDirectedEdge(two, three);
-g.addDirectedEdge(two, four);
-g.addDirectedEdge(three, five);
-g.addDirectedEdge(four, five);
-g.addDirectedEdge(one, three);
+// g.addDirectedEdge(zero, two);
+// g.addDirectedEdge(two, three);
+// g.addDirectedEdge(two, four);
+// g.addDirectedEdge(three, five);
+// g.addDirectedEdge(four, five);
+// g.addDirectedEdge(one, three);
 
-console.log(g.size());
-console.log(g.getVertices());
-console.log(g.getUniqueVertices());
-console.log(g.getNeighbors(three));
+// console.log(g.size());
+// console.log(g.getVertices());
+// console.log(g.getUniqueVertices());
+// console.log(g.getNeighbors(three));
+
+module.exports = {
+  Graph,
+  Vertex,
+  Edge
+};
--- code-challenges/graph/graph.test.js
+++ code-challenges/graph/graph.test.js
@@ -1,3 +1,39 @@
 'use strict';
 
-const { Vertex, Edge, Graph } = require('./graph');
+const { Vertex, Graph } = require('./graph');
+
+describe(':::: GRAPH ::::', () => {
+  let g, zero, one, two, twoToo, three, four, fourToo, five;
+  beforeEach(() => {
+    g = new Graph();
+
+    zero = new Vertex(0);
+    one = new Vertex(1);
+    two = new Vertex(2);
+    twoToo = new Vertex(2);
+    three = new Vertex(3);
+    four = new Vertex(4);
+    fourToo = new Vertex(4);
+    five = new Vertex(5);
+  });
+
+  test('#addVertex() adds vertex properly', () => {
+    expect(g.addVertex(zero)).toEqual([]);
+  });
+
+  test('#addDirectedEdge() adds edge properly', () => {
+    g.addVertex(zero);
+    g.addVertex(one);
+    g.addDirectedEdge(zero, one, '🦔');
+    expect(g.adjacencyList.get(zero)[0].vertex.value).toEqual(1);
+    expect(g.adjacencyList.get(zero)[0].weight).toEqual('🦔');
+  });
+
+  test('#addDirectedEdge() throws Error on adding edges to non-existent vertices', () => {
+    g.addVertex(zero);
+    g.addVertex(one);
+    expect(() => g.addDirectedEdge(two, three, '🦔')).toThrow(
+      'NON-EXISTENT VERTEX SITUATION 😱'
+    );
+  });
+});
